d9e4f266b3ba721aef9a4799b8e74c8d0e0312b8
Ammar HDAIFEH <ammar.hdaifeh@gmail.com>
2023-04-26 15:30:45 +0200

    update image size

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php
-// Start a session
 session_start();
-
-// Check if the user is not logged in
-if (!isset($_SESSION["username"])) {
-	// Redirect the user to the login page
-	header("Location: login.php");
-	exit();
-}
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
 ?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My Web Page with Image</title>
+    <title>My Web Page with Image</title>
 </head>
 <body>
-	<h1>Welcome to my web page!</h1>
-	<p>Here is an image:</p>
-	<img src="my_image.jpg.jpg" alt="My Image">
+    <h1>Welcome to my web page, <?php echo $username; ?>!</h1>
+    <p>Here is an image:</p>
+    <img src="my_image.jpg" alt="My Image" width="200" height="200">
+    <a href="logout.php">Logout</a>
 </body>
 </html>
+<?php
+} else {
+    header("Location: login.php");
+}
+?>
